d9e684b989579ee5a32c3eff01448bf46733a5bf
Gholbin <thibault.groot@gmail.com>
2020-02-03 13:23:37 +0100

    features-vecteur : ajout de la classe vecteur

---
+++ utils/Vecteurs.py
@@ -0,0 +1,19 @@
+import math
+
+
+class Vecteur:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def unite(self):
+        norme = math.sqrt(self.x**2 + self.y**2)
+        return Vecteur(self.x / norme, self.y / norme)
+
+    def __getitem__(self, key):
+        if key == 0:
+            return self.x
+        elif key == 1:
+            return self.y
+        else:
+            return None

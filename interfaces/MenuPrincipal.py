1b52dc7812f05b92f56263129f885ab2647ccdc6
ByLycheeZ <theo_p@orange.fr>
2020-02-03 20:48:02 +0100

    Evenements déplacés dans les classes des boutons + ajout du bouton crédits

--- interfaces/MenuPrincipal.py
+++ interfaces/MenuPrincipal.py
@@ -1,9 +1,8 @@
 import pygame
 
+from interfaces.elements.BoutonCredits import BoutonCredits
 from interfaces.elements.BoutonJouer import BoutonJouer
 from interfaces.elements.BoutonQuitter import BoutonQuitter
-
-from gestionnaires.Evenement import *
 
 
 class MenuPrincipal:
@@ -12,18 +11,10 @@ class MenuPrincipal:
         self.title = "Windy Course"
         self.bouton_jouer = BoutonJouer("Jouer", (350, 400))
         self.bouton_quitter = BoutonQuitter("Quitter", (350, 500))
-        # self.credits = BoutonCredits()
-        Evenement().enregistrer(pygame.MOUSEBUTTONUP, self)
+        self.bouton_credits = BoutonCredits("Credits", (910, 730))
 
     def afficher(self, ecran):
         ecran.blit(self.background, (-490, 0))
         self.bouton_jouer.afficher(ecran)
         self.bouton_quitter.afficher(ecran)
-
-    def evenement(self, evenement):
-        if (self.bouton_jouer.coord[0] <= pygame.mouse.get_pos()[0] <= self.bouton_jouer.coord[0] + 300) \
-                and (self.bouton_jouer.coord[1] <= pygame.mouse.get_pos()[1] <= self.bouton_jouer.coord[1] + 76):
-            print("Jouer")
-        elif (self.bouton_quitter.coord[0] <= pygame.mouse.get_pos()[0] <= self.bouton_quitter.coord[0] + 300) \
-                and (self.bouton_quitter.coord[1] <= pygame.mouse.get_pos()[1] <= self.bouton_quitter.coord[1] + 76):
-            quit()
+        self.bouton_credits.afficher(ecran)
--- interfaces/elements/BoutonJouer.py
+++ interfaces/elements/BoutonJouer.py
@@ -1,12 +1,19 @@
 import pygame
 
+from gestionnaires.Evenement import Evenement
 from interfaces.elements.Bouton import Bouton
 
 
 class BoutonJouer(Bouton):
     def __init__(self, label, coord):
         super().__init__(label, coord)
+        Evenement().enregistrer(pygame.MOUSEBUTTONUP, self)
 
     def afficher(self, ecran):
         image = pygame.image.load("res/bouton-jouer.png")
         ecran.blit(image, self.coord)
+
+    def evenement(self, evenement):
+        if (self.coord[0] <= pygame.mouse.get_pos()[0] <= self.coord[0] + 300) \
+                and (self.coord[1] <= pygame.mouse.get_pos()[1] <= self.coord[1] + 76):
+            print("Jouer")

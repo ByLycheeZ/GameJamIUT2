e679638c763ffb32e4e32e1078b90ccfa1744e70
LEDOYEN Baptiste <ledoyenb@hostname.iut2.upmf-grenoble.fr>
2020-02-04 14:57:51 +0100

    fix-Joueur: mise en attribut privee

--- entites/Joueur.py
+++ entites/Joueur.py
@@ -14,20 +14,20 @@ class Joueur:
     NB_SAUT_MAX = 1
 
     def __init__(self):
-        self.sprite = pygame.image.load(self.CHEMIN_SPRITE + 'dino-jaune.png')
-        self.rect = self.sprite.get_rect()
-        self.rect.y = HEIGHT - self.TAILLE_IMAGE[1]
-        self.vitesse = 300
-        self.deplacement = [0, 0]
-        self.velocite_saut, self.vitesse_chute = 2, 4
-        self.nb_saut_restant = 1
+        self.__sprite = pygame.image.load(self.CHEMIN_SPRITE + 'dino-jaune.png')
+        self.__rect = self.__sprite.get_rect()
+        self.__rect.y = HEIGHT - self.TAILLE_IMAGE[1]
+        self.__vitesse = 300
+        self.__deplacement = [0, 0]
+        self.__velocite_saut, self.vitesse_chute = 2, 4
+        self.__nb_saut_restant = 1
 
-        self.anim_attente = Animation(0, 0, Joueur.TAILLE_IMAGE[0], Joueur.TAILLE_IMAGE[1], 4, 0.2)
-        self.anim_deplacement = Animation(4, 0, Joueur.TAILLE_IMAGE[0], Joueur.TAILLE_IMAGE[1], 6, 0.2)
-        self.anim_attaque = Animation(10, 0, Joueur.TAILLE_IMAGE[0], Joueur.TAILLE_IMAGE[1], 3, 0.2)
-        self.anim_degat = Animation(13, 0, Joueur.TAILLE_IMAGE[0], Joueur.TAILLE_IMAGE[1], 4, 0.2)
-        self.anim_accroupi = Animation(18, 0, Joueur.TAILLE_IMAGE[0], Joueur.TAILLE_IMAGE[1], 6, 0.2)
-        self.anim_active = self.anim_attente
+        self.__anim_attente = Animation(0, 0, Joueur.TAILLE_IMAGE[0], Joueur.TAILLE_IMAGE[1], 4, 0.2)
+        self.__anim_deplacement = Animation(4, 0, Joueur.TAILLE_IMAGE[0], Joueur.TAILLE_IMAGE[1], 6, 0.2)
+        self.__anim_attaque = Animation(10, 0, Joueur.TAILLE_IMAGE[0], Joueur.TAILLE_IMAGE[1], 3, 0.2)
+        self.__anim_degat = Animation(13, 0, Joueur.TAILLE_IMAGE[0], Joueur.TAILLE_IMAGE[1], 4, 0.2)
+        self.__anim_accroupi = Animation(18, 0, Joueur.TAILLE_IMAGE[0], Joueur.TAILLE_IMAGE[1], 6, 0.2)
+        self.__anim_active = self.__anim_attente
 
         evenement = Evenement()
         evenement.enregistrer(pygame.KEYDOWN, self)
@@ -38,81 +38,81 @@ class Joueur:
     def evenement(self, evenement):
         if evenement.type == pygame.KEYDOWN:
             if evenement.key == pygame.K_q:
-                self.deplacement[0] -= 1
+                self.__deplacement[0] -= 1
             elif evenement.key == pygame.K_d:
-                self.deplacement[0] += 1
-            elif evenement.key == pygame.K_SPACE and self.nb_saut_restant > 0:
-                self.deplacement[1] -= self.velocite_saut
-                self.nb_saut_restant -= 1
+                self.__deplacement[0] += 1
+            elif evenement.key == pygame.K_SPACE and self.__nb_saut_restant > 0:
+                self.__deplacement[1] -= self.__velocite_saut
+                self.__nb_saut_restant -= 1
 
         else:  # KEYUP
             if evenement.key == pygame.K_q:
-                self.deplacement[0] += 1
+                self.__deplacement[0] += 1
             elif evenement.key == pygame.K_d:
-                self.deplacement[0] -= 1
+                self.__deplacement[0] -= 1
 
-        if self.deplacement[0] != 0:
-            self.anim_active = self.anim_deplacement
+        if self.__deplacement[0] != 0:
+            self.__anim_active = self.__anim_deplacement
         else:
-            self.anim_active = self.anim_attente
+            self.__anim_active = self.__anim_attente
 
     def maj(self, delta):
-        self.anim_active.ajouter_temps(delta)
-        self.rect = self.rect.move(self.vitesse * self.deplacement[0] * delta,
-                                   self.vitesse * self.deplacement[1] * delta)
+        self.__anim_active.ajouter_temps(delta)
+        self.__rect = self.__rect.move(self.__vitesse * self.__deplacement[0] * delta,
+                                       self.__vitesse * self.__deplacement[1] * delta)
 
-        self.deplacement[1] += self.vitesse_chute * delta
+        self.__deplacement[1] += self.vitesse_chute * delta
         # temporaire ne peut pas tomber dans le vide
-        if self.rect.y >= 768 - 120:
-            self.rect.y = 768 - 120
-            self.deplacement[1] = 0
+        if self.__rect.y >= 768 - 120:
+            self.__rect.y = 768 - 120
+            self.__deplacement[1] = 0
             self.ajout_saut()
 
-        droite = self.rect.left + self.TAILLE_IMAGE[0]
+        droite = self.__rect.left + self.TAILLE_IMAGE[0]
         if Ecran.get_droite() - droite < 10:
             Ecran.deplacement(droite - Ecran.largeur + 10, Ecran.y)
 
-        Parallax().deplacement_joueur(self.deplacement[0], delta)
+        Parallax().deplacement_joueur(self.__deplacement[0], delta)
 
     def affichage(self, ecran):
-        sous_sprite = self.sprite.subsurface(self.anim_active.recuperer_image())
+        sous_sprite = self.__sprite.subsurface(self.__anim_active.recuperer_image())
         sous_sprite_rect = sous_sprite.get_rect()
-        sous_sprite_rect.x, sous_sprite_rect.y = self.rect.x, self.rect.y
-        ecran.blit(pygame.transform.flip(sous_sprite, self.deplacement[0] < 0, False), sous_sprite_rect)
+        sous_sprite_rect.x, sous_sprite_rect.y = self.__rect.x, self.__rect.y
+        ecran.blit(pygame.transform.flip(sous_sprite, self.__deplacement[0] < 0, False), sous_sprite_rect)
 
     def get_vitesse(self):
-        return self.vitesse
+        return self.__vitesse
 
     def get_sprite(self):
-        return self.sprite
+        return self.__sprite
 
     def get_rect(self):
-        return self.rect
+        return self.__rect
 
     def get_deplacement(self):
-        return self.deplacement
+        return self.__deplacement
 
     def get_nb_saut_restant(self):
-        return self.nb_saut_restant
+        return self.__nb_saut_restant
 
     def get_nb_saut_max(self):
         return self.NB_SAUT_MAX
 
     def set_vitesse(self, vitesse):
-        self.vitesse = vitesse
+        self.__vitesse = vitesse
 
     def set_sprite(self, nom_fichier):
-        self.sprite = pygame.image.load(self.CHEMIN_SPRITE + nom_fichier)
+        self.__sprite = pygame.image.load(self.CHEMIN_SPRITE + nom_fichier)
 
     def set_rect(self, rect):
-        self.rect = rect
+        self.__rect = rect
 
     def set_velocite_saut(self, velocite_saut):
-        self.velocite_saut = velocite_saut
+        self.__velocite_saut = velocite_saut
 
     def set_vitesse_chute(self, vitesse_chute):
         self.vitesse_chute = vitesse_chute
 
     def ajout_saut(self, nb=1):
-        self.nb_saut_restant = min([nb + self.nb_saut_restant, self.NB_SAUT_MAX])
+        self.__nb_saut_restant = min([nb + self.__nb_saut_restant, self.NB_SAUT_MAX])
 

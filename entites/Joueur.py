494585339d192535eafb0c585b3b6d0c226c6b1b
LEDOYEN Baptiste <ledoyenb@hostname.iut2.upmf-grenoble.fr>
2020-02-04 13:10:04 +0100

    Gestions des sauts du Joueur Creations des get et set de la classe player

--- entites/Joueur.py
+++ entites/Joueur.py
@@ -3,16 +3,22 @@ from gestionnaires.Affichage import *
 from gestionnaires.Maj import *
 from gestionnaires.Evenement import *
 from utils.Animation import Animation
+from utils.Constantes import *
 
 
 class Joueur:
-    TAILLE_IMAGE = [24 * 5, 24 * 5]
+    TAILLE_IMAGE = [120, 120]
+    CHEMIN_SPRITE = 'res/img/'
+    NB_SAUT_MAX = 1
 
     def __init__(self):
-        self.sprite = pygame.image.load('res/img/dino-jaune.png')
+        self.sprite = pygame.image.load(self.CHEMIN_SPRITE + 'dino-jaune.png')
         self.rect = self.sprite.get_rect()
+        self.rect.y = HEIGHT - self.TAILLE_IMAGE[1]
         self.vitesse = 300
         self.deplacement = [0, 0]
+        self.saut = self.velocite, self.vitesse_chute = 2, 4
+        self.nb_saut_restant = 1
 
         self.anim_attente = Animation(0, 0, Joueur.TAILLE_IMAGE[0], Joueur.TAILLE_IMAGE[1], 4, 0.2)
         self.anim_deplacement = Animation(4, 0, Joueur.TAILLE_IMAGE[0], Joueur.TAILLE_IMAGE[1], 6, 0.2)
@@ -33,6 +39,9 @@ class Joueur:
                 self.deplacement[0] -= 1
             elif evenement.key == pygame.K_d:
                 self.deplacement[0] += 1
+            elif evenement.key == pygame.K_SPACE and self.nb_saut_restant > 0:
+                self.deplacement[1] -= self.velocite
+                self.nb_saut_restant -= 1
 
         else:  # KEYUP
             if evenement.key == pygame.K_q:
@@ -40,7 +49,7 @@ class Joueur:
             elif evenement.key == pygame.K_d:
                 self.deplacement[0] -= 1
 
-        if self.deplacement[0] != 0 or self.deplacement[1] != 0:
+        if self.deplacement[0] != 0:
             self.anim_active = self.anim_deplacement
         else:
             self.anim_active = self.anim_attente
@@ -50,8 +59,52 @@ class Joueur:
         self.rect = self.rect.move(self.vitesse * self.deplacement[0] * delta,
                                    self.vitesse * self.deplacement[1] * delta)
 
+        self.deplacement[1] += self.vitesse_chute * delta
+        # temporaire ne peut pas tomber dans le vide
+        if self.rect.y >= 768 - 120:
+            self.rect.y = 768 - 120
+            self.deplacement[1] = 0
+            self.ajoutSaut()
+
     def affichage(self, ecran):
         sous_sprite = self.sprite.subsurface(self.anim_active.recuperer_image())
         sous_sprite_rect = sous_sprite.get_rect()
         sous_sprite_rect.x, sous_sprite_rect.y = self.rect.x, self.rect.y
         ecran.blit(pygame.transform.flip(sous_sprite, self.deplacement[0] < 0, False), sous_sprite_rect)
+
+    def get_vitesse(self):
+        return self.vitesse
+
+    def get_sprite(self):
+        return self.sprite
+
+    def get_rect(self):
+        return self.rect
+
+    def get_deplacement(self):
+        return self.deplacement
+
+    def get_saut(self):
+        return self.saut
+
+    def get_nb_saut_restant(self):
+        return self.nb_saut_restant
+
+    def get_nb_saut_max(self):
+        return self.NB_SAUT_MAX
+
+    def set_vitesse(self, vitesse):
+        self.vitesse = vitesse
+
+    def set_sprite(self, nom_fichier):
+        self.sprite = pygame.image.load(self.CHEMIN_SPRITE + nom_fichier)
+
+    def set_rect(self, rect):
+        self.rect = rect
+
+    def set_saut(self, saut):
+        self.saut = saut
+
+    def ajoutSaut(self, nb=1):
+        if nb <= self.NB_SAUT_MAX and self.nb_saut_restant < self.NB_SAUT_MAX:
+            self.nb_saut_restant += 1

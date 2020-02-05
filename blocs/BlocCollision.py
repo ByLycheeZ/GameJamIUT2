8fad0a88955db5c3facad927dafb4a179037df23
Feelzor <losdingtro@gmail.com>
2020-02-05 14:15:07 +0100

    feature-carte: gestion des blocs de collision

--- blocs/BlocCollision.py
+++ blocs/BlocCollision.py
@@ -6,13 +6,13 @@ from blocs.Bloc import Bloc
 
 class BlocCollision(Bloc):
 
-    def __init__(self, json, x, y):
-        Bloc.__init(json, x, y)
+    def __init__(self, json, x, y, taille=1):
+        super(BlocCollision, self).__init__(json, x, y, taille)
 
     def get_collisions(self, rect):
-        this_rect = self.dessin.get_rect()
+        this_rect = self.__dessin.get_rect()
         collision = [0, 0]
-        if self.dessin.get_rect().colliderect(rect):
+        if self.__dessin.get_rect().colliderect(rect):
             if (this_rect.left < rect.left and this_rect.right > rect.right) or (this_rect.left > rect.left and this_rect.right > rect.right):
                 collision[0] = -1
             if (this_rect.top < rect.top and this_rect.bottom > rect.bottom) or (this_rect.top > rect.top and this_rect.bottom < rect.bottom) :
--- blocs/Plateforme.py
+++ blocs/Plateforme.py
@@ -1,11 +1,15 @@
 from blocs.Bloc import Bloc
+from blocs.BlocCollision import BlocCollision
 from gestionnaires.Affichage import Affichage
 
 
 class Plateforme:
     class __Elem:
-        def __init__(self, nom, x, y, taille):
-            self.bloc = Bloc(nom, 0, 0, taille)
+        def __init__(self, nom, x, y, taille, collision):
+            if collision:
+                self.bloc = BlocCollision(nom, 0, 0, taille)
+            else:
+                self.bloc = Bloc(nom, 0, 0, taille)
             Affichage().supprimer(self.bloc)
             self.x = x
             self.y = y
@@ -14,7 +18,7 @@ class Plateforme:
         self.__blocs = []
         self.__x_max = 0
         for bloc in donnees:
-            elem = Plateforme.__Elem(bloc['source'], bloc['x'] + x, bloc['y'] + y, taille)
+            elem = Plateforme.__Elem(bloc['source'], bloc['x'] + x, bloc['y'] + y, taille, bloc['collision'])
             self.__blocs.append(elem)
             self.__x_max = max([self.__x_max, elem.x + elem.bloc.get_largeur()])
 

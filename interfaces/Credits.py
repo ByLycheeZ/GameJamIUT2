d90b915efe71ced9b4a3315dc86867158717b985
PONCHON Theo <ponchoth@hostname.iut2.upmf-grenoble.fr>
2020-02-05 13:05:06 +0100

    Fix-credits : Interface des crédits totalement fonctionnelle à compléter si besoin et couleur à modifier potentiellement

--- interfaces/Credits.py
+++ interfaces/Credits.py
@@ -39,28 +39,32 @@ class Credits:
                 # Noms
                 font = pygame.font.Font("res/fonts/Comfortaa-Bold.ttf", 36)
                 ecran.blit(font.render("Baptiste LEDOYEN", True, self.BLEU), (30, 170))
-                ecran.blit(font.render("Thibault GROOT", True, self.BLEU), (30, 350))
-                ecran.blit(font.render("Théo PONCHON", True, self.BLEU), (650, 170))
-                ecran.blit(font.render("Thomas VINCENT", True, self.BLEU), (650, 350))
-                ecran.blit(font.render("Léo LE CORRE", True, self.BLEU), (Constantes.LARGEUR/2 - texte.get_width()/2, 530))
+                ecran.blit(font.render("Thomas VINCENT", True, self.BLEU), (30, 350))
+                ecran.blit(font.render("Thibault GROOT", True, self.BLEU), (670, 350))
+                ecran.blit(font.render("Théo PONCHON", True, self.BLEU), (670, 170))
+                texte = font.render("Léo LE CORRE", True, self.BLEU)
+                ecran.blit(texte, (Constantes.LARGEUR/2 - texte.get_width()/2, 530))
 
                 # Rôles
                 font = pygame.font.Font("res/fonts/Comfortaa-Bold.ttf", 26)
                 # Baptiste
                 ecran.blit(font.render("Chef de projet", True, self.BLANC), (90, 220))
                 ecran.blit(font.render("Développeur", True, self.BLANC), (90, 260))
-                # Théo
-                ecran.blit(font.render("Développeur UI", True, self.BLANC), (700, 220))
-                ecran.blit(font.render("Designer", True, self.BLANC), (700, 260))
-                # Thibault
-                ecran.blit(font.render("Développeur", True, self.BLANC), (90, 260))
                 # Thomas
-                ecran.blit(font.render("Développeur", True, self.BLANC), (90, 260))
+                ecran.blit(font.render("Responsable technique", True, self.BLANC), (30, 400))
+                ecran.blit(font.render("Développeur", True, self.BLANC), (90, 440))
+                # Thibault
+                ecran.blit(font.render("Développeur", True, self.BLANC), (720, 400))
+                # Théo
+                ecran.blit(font.render("Développeur UI", True, self.BLANC), (720, 220))
+                ecran.blit(font.render("Designer", True, self.BLANC), (720, 260))
+                # Léo
+                texte = font.render("Game Designer", True, self.BLANC)
+                ecran.blit(texte, (Constantes.LARGEUR/2 - texte.get_width()/2 + 30, 580))
 
                 # Bouton Sources
                 font = pygame.font.Font("res/fonts/Comfortaa-Bold.ttf", 30)
-                texte = font.render("Sources", True, self.TURQUOISE)
-                ecran.blit(texte, (880, 720))
+                ecran.blit(font.render("Sources", True, self.TURQUOISE), (880, 720))
 
             elif self.sources and not self.equipe:
                 image = pygame.image.load("res/img/interfaces/credits/images-sons.png")
@@ -72,8 +76,20 @@ class Credits:
 
                 # Bouton Equipe
                 font = pygame.font.Font("res/fonts/Comfortaa-Bold.ttf", 30)
-                texte = font.render("Equipe", True, self.TURQUOISE)
-                ecran.blit(texte, (10, 720))
+                ecran.blit(font.render("Equipe", True, self.TURQUOISE), (10, 720))
+
+                # Ressources
+                font = pygame.font.Font("res/fonts/Comfortaa-Bold.ttf", 20)
+                ecran.blit(font.render("Dinosaures", True, self.BLEU), (70, 200))
+                ecran.blit(font.render("Son de mort", True, self.BLEU), (70, 250))
+                ecran.blit(font.render("Coeur HUD", True, self.BLEU), (70, 300))
+                ecran.blit(font.render("Son de crash (pour la fin du jeu)", True, self.BLEU), (70, 350))
+
+                # Liens
+                ecran.blit(font.render("https://arks.itch.io/dino-characters", True, self.BLANC), (250, 200))
+                ecran.blit(font.render("https://opengameart.org/content/15-monster-gruntpaindeath-sounds", True, self.BLANC), (250, 250))
+                ecran.blit(font.render("https://opengameart.org/content/pixel-hearts", True, self.BLANC), (250, 300))
+                ecran.blit(font.render("https://opengameart.org/content/crash-collision", True, self.BLANC), (430, 350))
 
             # Instruction ESC
             font = pygame.font.Font("res/fonts/Comfortaa-Bold.ttf", 20)

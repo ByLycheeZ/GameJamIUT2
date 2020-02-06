9feb93982bfc88534e3869460d45e8ae087284fc
Feelzor <losdingtro@gmail.com>
2020-02-06 10:46:02 +0100

    feature-courants: modification de la vitesse du courant

--- blocs/BlocCourant.py
+++ blocs/BlocCourant.py
@@ -2,17 +2,17 @@ from blocs.BlocCollision import BlocCollision
 
 
 class BlocCourant(BlocCollision):
-    VITESSE = 10
 
-    def __init__(self, json, x, y, taille, direction):
+    def __init__(self, json, x, y, taille, direction, vitesse):
         super(BlocCourant, self).__init__(json, x, y, taille)
         self.__direction = direction
+        self.__vitesse = vitesse
 
     def collisions(self, joueur, delta):
         this_rect = self._dessin.get_rect()
         this_rect.x, this_rect.y = self._x, self._y
 
         if this_rect.colliderect(joueur.get_rect_collision()):
-            joueur.ajouter_boost(*[self.VITESSE * delta * x for x in self.__direction])
+            joueur.ajouter_boost(*[self.__vitesse * delta * x for x in self.__direction])
 
         return None

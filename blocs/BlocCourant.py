94154c02f6ccbf81e12f6bfa768e8844de66975c
Feelzor <losdingtro@gmail.com>
2020-02-06 17:23:07 +0100

    feature-son-vent: ajout du son de vent lors de l'entrée dans le bloc de vent

--- blocs/BlocCourant.py
+++ blocs/BlocCourant.py
@@ -1,4 +1,5 @@
 from blocs.BlocCollision import BlocCollision
+from gestionnaires.Sons import Sons
 
 
 class BlocCourant(BlocCollision):
@@ -7,6 +8,7 @@ class BlocCourant(BlocCollision):
         super(BlocCourant, self).__init__(json, x, y, taille)
         self.__direction = direction
         self.__vitesse = vitesse
+        self.__joueurs = []
 
     def collisions(self, joueur, delta):
         this_rect = self.get_rect()
@@ -14,5 +16,10 @@ class BlocCourant(BlocCollision):
 
         if this_rect.colliderect(joueur.get_rect_collision()):
             joueur.ajouter_boost(*[self.__vitesse * delta * x for x in self.__direction])
+            if joueur not in self.__joueurs:
+                Sons().jouer_son('vent')
+                self.__joueurs.append(joueur)
+        elif joueur in self.__joueurs:
+            self.__joueurs.remove(joueur)
 
         return None
